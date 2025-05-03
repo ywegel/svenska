7990ebb3c9996b215cd6d95f210879017d156d62
ywegel <dev@ywegel.de>
2025-05-03 11:52:14 +0200

    Clicking on a .json file allows the user to open it in the app. The app deepLinks this action to the WordImporterScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = true
             proguardFiles(
